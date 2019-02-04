88cdf4f21b89086db731cca859805513a8b42b7e
Ashley Archibald <aarchibald20@gmail.com>
2019-02-04 05:56:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ if phrase == "I LOVE YOU GRANDMA"
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
